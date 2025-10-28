69b7e508103275c0a6c4659bba354fea22b615a6
hyein89 <bihontai@gmail.com>
2025-10-28 23:36:23 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Head from 'next/head';
-import '../styles/custom.css';
+
 
 export default function Home() {
   const [offerUrl, setOfferUrl] = useState('');
